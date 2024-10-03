febf05f10b7f9f8184e70b3091e45d59ce8ca470
fornari03 <221017023@aluno.unb.br>
2024-10-03 16:04:08 -0300

    feat: add spring security

---
+++ src/main/java/br/dev/todo_list_spring/security/SecurityConfig.java
@@ -0,0 +1,43 @@
+package br.dev.todo_list_spring.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/").permitAll()  // endpoints públicos
+                .anyRequest().authenticated()  // o restante requer autenticação
+            )
+            .formLogin(form -> form.defaultSuccessUrl("/swagger-ui/index.html", true))  // login default do Spring Security
+            .logout((logout) -> logout.permitAll());  // logout padrão
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+            .username("user")
+            .password("password")
+            .roles("USER")
+            .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
+
