94023b4579d6afdf85c4134f256a8e7c07574133
fornari03 <221017023@aluno.unb.br>
2024-10-03 11:27:35 -0300

    refac: update DTOs to records

--- src/main/java/br/dev/todo_list_spring/dto/UserDTO.java
+++ src/main/java/br/dev/todo_list_spring/dto/UserDTO.java
@@ -1,12 +1,18 @@
 package br.dev.todo_list_spring.dto;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import br.dev.todo_list_spring.model.User;
 
-@NoArgsConstructor
-@Data
-public class UserDTO {
-    private Long id;
-    private String username;
+public record UserDTO(Long id, String username) {
+    public UserDTO(User user) {
+        this(user.getId(), user.getUsername());
+    }
+
+    public User toEntity() {
+        User user = new User();
+        user.setId(this.id());
+        user.setUsername(this.username());
+        return user;
+    }
 }
+
 
