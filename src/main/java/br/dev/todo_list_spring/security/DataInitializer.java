a91880217c74ca32ab862015aca0a504f57e6732
fornari03 <221017023@aluno.unb.br>
2024-10-03 17:23:17 -0300

    feat: add SecurityConfig

---
+++ src/main/java/br/dev/todo_list_spring/security/DataInitializer.java
@@ -0,0 +1,27 @@
+package br.dev.todo_list_spring.security;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import br.dev.todo_list_spring.model.User;
+import br.dev.todo_list_spring.service.UserService;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    private final UserService userService;
+
+    public DataInitializer(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public void run(String... args) {
+        User adminUser = new User();
+        adminUser.setUsername("admin");
+        adminUser.setPassword("admin123");
+        adminUser.setRole("ADMIN");
+
+        userService.create(adminUser);
+    }
+}
--- src/main/java/br/dev/todo_list_spring/security/SecurityConfig.java
+++ src/main/java/br/dev/todo_list_spring/security/SecurityConfig.java
@@ -1,7 +1,9 @@
 package br.dev.todo_list_spring.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -11,33 +13,33 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.authentication.AuthenticationManager;
+
+
+import br.dev.todo_list_spring.service.UserService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final UserService userService;
+
+    public SecurityConfig(UserService userService) {
+        this.userService = userService;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/").permitAll()  // endpoints públicos
+                .requestMatchers("/", "/swagger-ui/index.html/users/register").permitAll()  // endpoints públicos
                 .anyRequest().authenticated()  // o restante requer autenticação
             )
-            .formLogin(form -> form.defaultSuccessUrl("/swagger-ui/index.html", true))  // login default do Spring Security
+            .formLogin(form -> form.defaultSuccessUrl("/swagger-ui/index.html", true))  // redireciona para o swagger após o login
             .logout((logout) -> logout.permitAll());  // logout padrão
 
         return http.build();
     }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-            .username("user")
-            .password("password")
-            .roles("USER")
-            .build();
-
-        return new InMemoryUserDetailsManager(user);
-    }
 }
+
 
