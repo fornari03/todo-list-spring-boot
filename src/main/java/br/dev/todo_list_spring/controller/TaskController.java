8be0a643111d91e18043883627469131f74d3343
fornari03 <221017023@aluno.unb.br>
2024-10-03 12:00:36 -0300

    feat: update TaskController and add creatTask

--- src/main/java/br/dev/todo_list_spring/controller/TaskController.java
+++ src/main/java/br/dev/todo_list_spring/controller/TaskController.java
@@ -5,28 +5,41 @@ import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.dev.todo_list_spring.dto.TaskDTO;
 import br.dev.todo_list_spring.model.Task;
+import br.dev.todo_list_spring.model.User;
 import br.dev.todo_list_spring.service.TaskService;
+import br.dev.todo_list_spring.service.UserService;
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
     private final TaskService taskService;
+    private final UserService userService;
 
-    public TaskController(TaskService taskService) {
+    public TaskController(TaskService taskService, UserService userService) {
         this.taskService = taskService;
+        this.userService = userService;
     }
 
-    @GetMapping
-    public ResponseEntity<List<TaskDTO>> getUserTasks() {
-        List<Task> tasks = taskService.getTasksByUser("test");
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<TaskDTO>> findTasksByUserId(@PathVariable Long userId) {
+        List<Task> tasks = taskService.getTasksByUserId(userId);
         List<TaskDTO> taskDTOs = tasks.stream()
-                                      .map(TaskDTO::toDTO)
+                                      .map(TaskDTO::new)
                                       .collect(Collectors.toList());
         return ResponseEntity.ok(taskDTOs);
+    }
+
+    @PostMapping("/{userId}")
+    public ResponseEntity<TaskDTO> createTask(@PathVariable Long userId, @RequestBody TaskDTO taskDTO) {
+        Task task = taskDTO.toEntity(userService.findById(userId));
+        return ResponseEntity.ok(new TaskDTO(taskService.create(task)));
     }
 }
