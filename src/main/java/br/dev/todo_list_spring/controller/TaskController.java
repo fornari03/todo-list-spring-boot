f60949bff99c475f41251bf672701123989ef299
fornari03 <221017023@aluno.unb.br>
2024-10-04 16:46:01 -0300

    feat: TaskController methods need authentication from user

--- src/main/java/br/dev/todo_list_spring/controller/TaskController.java
+++ src/main/java/br/dev/todo_list_spring/controller/TaskController.java
@@ -1,15 +1,22 @@
 package br.dev.todo_list_spring.controller;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import br.dev.todo_list_spring.dto.TaskDTO;
 import br.dev.todo_list_spring.model.Task;
@@ -22,6 +29,7 @@ import br.dev.todo_list_spring.service.UserService;
 public class TaskController {
     private final TaskService taskService;
     private final UserService userService;
+    private Long loggedUserId = -1L;
 
     public TaskController(TaskService taskService, UserService userService) {
         this.taskService = taskService;
@@ -29,8 +37,9 @@ public class TaskController {
     }
 
     @GetMapping("/{userId}")
-    public ResponseEntity<List<TaskDTO>> findTasksByUserId(@PathVariable Long userId) {
-        List<Task> tasks = taskService.getTasksByUserId(userId);
+    public ResponseEntity<List<TaskDTO>> findTasksByUserId(Principal principal) {
+        loggedUserId = loggedUserId == -1L ? userService.findByUsername(principal.getName()).getId() : loggedUserId;
+        List<Task> tasks = taskService.findByUserId(loggedUserId);
         List<TaskDTO> taskDTOs = tasks.stream()
                                       .map(TaskDTO::new)
                                       .collect(Collectors.toList());
@@ -38,8 +47,9 @@ public class TaskController {
     }
 
     @PostMapping("/{userId}")
-    public ResponseEntity<TaskDTO> createTask(@PathVariable Long userId, @RequestBody TaskDTO taskDTO) {
-        Task task = taskDTO.toEntity(userService.findById(userId));
+    public ResponseEntity<TaskDTO> createTask(Principal principal, @RequestBody TaskDTO taskDTO) {
+        loggedUserId = loggedUserId == -1L ? userService.findByUsername(principal.getName()).getId() : loggedUserId;
+        Task task = taskDTO.toEntity(userService.findById(loggedUserId));
         return ResponseEntity.ok(new TaskDTO(taskService.create(task)));
     }
 }
