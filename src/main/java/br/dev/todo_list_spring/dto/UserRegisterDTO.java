656db7a69c9d6cf501138e30feba7f010d3b9c39
fornari03 <221017023@aluno.unb.br>
2024-10-04 18:04:53 -0300

    fix: UserRegisterDTO

--- src/main/java/br/dev/todo_list_spring/dto/UserRegisterDTO.java
+++ src/main/java/br/dev/todo_list_spring/dto/UserRegisterDTO.java
@@ -2,12 +2,12 @@ package br.dev.todo_list_spring.dto;
 
 import br.dev.todo_list_spring.model.User;
 
-public record UserRegisterDTO(Long id, String username, String password) {
+public record UserRegisterDTO(String username, String password, String role) {
     public User toEntity() {
         User user = new User();
-        user.setId(this.id());
         user.setUsername(this.username());
         user.setPassword(this.password());
+        user.setRole(role);
         return user;
     }
 }
